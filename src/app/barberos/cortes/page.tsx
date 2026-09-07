37b43c1fb4fd0faa938c1d52203c3655f928854b
Yesid Casallas <casallas912@gmail.com>
2026-09-06 22:36:45 -0500

    Logros: Se agrega page de cortes

---
+++ src/app/barberos/cortes/page.tsx
@@ -0,0 +1,7 @@
+export default function CortePage(){
+    return (
+        <div>
+            <h1>Cortes</h1>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
+import Header from "@/components/layout/header/Header";
+import Footer from "@/components/layout/footer/Footer";
 import HeroSection from "@/components/sections/HeroSection";
 import { BarbersSection } from "@/components/sections/BarbersSection";
 
